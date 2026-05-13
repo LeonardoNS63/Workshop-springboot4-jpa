42fecaf130db02658321dd7b7bf3ed92269f7f29
Leonardo <leonardoserpa4343@gmail.com>
2026-05-12 21:59:19 -0300

    Exception handling - update

--- src/main/java/com/personal/aula/Spring/servicies/UserService.java
+++ src/main/java/com/personal/aula/Spring/servicies/UserService.java
@@ -4,6 +4,7 @@ import com.personal.aula.Spring.entities.User;
 import com.personal.aula.Spring.repositories.UserRepository;
 import com.personal.aula.Spring.servicies.exceptions.DatabaseException;
 import com.personal.aula.Spring.servicies.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -42,9 +43,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try{
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
@@ -52,4 +57,6 @@ public class UserService {
         entity.setEmail(obj.getEmail());
         entity.setPhone(obj.getPhone());
     }
+
 }
+
